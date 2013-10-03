b739abee32ce886589c6499b04936962bb879526
DenisSavenkov <denis.savenkov@emory.edu>
2013-10-03 13:00:55 -0400

    Fixing link replacement for # and javascript: href

--- httpproxy/decorators.py
+++ httpproxy/decorators.py
@@ -13,9 +13,10 @@ REWRITE_STYLES_REGEX = re.compile(r'((?:url)\(["\'])(.*?)(["\']\))',
 
 
 def rewrite_response(fn, base_url, proxy_view_name):
-    def fix_relative_url(url):
+    def fix_relative_url(proxy_url, url):
         if not url.startswith('javascript:') and not url.startswith('#'):
-            return urllib.quote(urljoin(base_url, url), safe=':=&;/')
+            return proxy_url + urllib.quote(urljoin(base_url, url),
+                safe=':=&;/')
         return url
     """
     Rewrites the response to fix references to resources loaded from HTML
@@ -28,10 +29,9 @@ def rewrite_response(fn, base_url, proxy_view_name):
         )
         def replace_links(match):
             href = match.group(1)
-            link_url = fix_relative_url(match.group(2))
+            link_url = fix_relative_url(proxy_root, match.group(2))
             quotes = match.group(3)
-            return (href + proxy_root + link_url + quotes) \
-                .encode(response._charset)
+            return (href + link_url + quotes).encode(response._charset)
         response.content = REWRITE_HTML_REGEX.sub(replace_links, 
             response.content)
         response.content = REWRITE_STYLES_REGEX.sub(replace_links,
